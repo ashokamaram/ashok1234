558776b17100c663709cf03e9e6326f85dca7314
root <root@ip-10-10-3-173.ec2.internal>
2019-05-15 15:53:40 +0000

    added username and password123

--- usercreate.sh
+++ usercreate.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "THIS SCCRIPT CREATE USER"
+read -p "please enter the user name:" USERNAME
+echo $USERNAME
+read -s -p "please enter the password:" PASSWORD
+useradd -m $USERNAME -P $PASSWORD
+
